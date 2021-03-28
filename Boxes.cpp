1e84d98f890b28151f923b56a5edbdcd80d711c5
Adam Owen <ahowenjr@alaska.edu>
2021-03-28 15:04:38 -0800

    Initial commit. added getWidth, getHeight

---
+++ Boxes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by A. Harrison Owen on 3/28/2021.
+//
+
+#include "Boxes.h"
---
+++ Boxes.h
@@ -0,0 +1,25 @@
+//
+// Created by A. Harrison Owen on 3/28/2021.
+//
+
+#ifndef CS202HW4_BOXES_H
+#define CS202HW4_BOXES_H
+
+
+class Boxes {
+public:
+
+    int getWidth() const {
+        return _width;
+    };
+    int getHeight(){
+        return _height;
+    };
+    
+private:
+    int _width;
+    int _height;
+};
+
+
+#endif //CS202HW4_BOXES_H
