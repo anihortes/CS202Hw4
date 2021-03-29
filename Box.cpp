bebd583cb274cda4f9afd715501b15e240de0ace
Adam Owen <ahowenjr@alaska.edu>
2021-03-28 19:10:17 -0800

    finished all versions. need to do pointer stuff next.

--- Box.cpp
+++ Box.cpp
@@ -12,3 +12,49 @@ void FilledBox::print(std::ostream &os) const{
         os << std::endl;
     }
 }
+
+void HollowBox::print(std::ostream &os) const {
+    for (auto i = 0; i < getHeight(); ++i){
+        if (i == 0 || i == getHeight()-1){
+            for(auto j = 0; j < getWidth(); ++j){
+                os << "x";
+            }
+        }
+        else {
+            for (auto j = 0; j < getWidth(); ++j) {
+                if( j == 0 || j == getWidth()-1){
+                    os << "x";
+                }
+                else {
+                    os << " ";
+                }
+            }
+        }
+        os << std::endl;
+    }
+}
+
+void CheckeredBox::print(std::ostream &os) const {
+    for(auto i = 0; i < getHeight(); ++i){
+        if(i%2 == 0){
+            for (auto j = 0; j < getWidth(); ++j){
+                if(j%2 == 0){
+                    os << "x";
+                }
+                else
+                    os << " ";
+            }
+        }
+
+        else {
+            for (auto j = 0; j < getWidth(); ++j){
+                if(j%2 == 0){
+                    os << " ";
+                }
+                else
+                    os << "x";
+            }
+        }
+        os << std::endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -7,5 +7,17 @@ int main() {
     test.setHeight(4);
     test.setWidth(4);
     test.print(std::cout);
+
+    HollowBox hollow;
+    hollow.print(std::cout);
+    hollow.setWidth(5);
+    hollow.setHeight(5);
+    hollow.print(std::cout);
+
+    CheckeredBox check;
+    check.print(std::cout);
+    check.setHeight(6);
+    check.setWidth(6);
+    check.print(std::cout);
     return 0;
 }
